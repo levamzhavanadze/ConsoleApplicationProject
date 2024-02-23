4ba79f85cb4090095dd69a87c4b3e90c0d787c1b
Levan Mzhavanadze <levamzhavanadze@gmail.com>
2024-02-23 14:26:01 +0400

    Calculator

---
+++ ConsoleApplicationProject/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationProject
+{
+    internal class Calculator
+    {
+        //public decimal _a;
+        //public decimal _b;
+
+
+        public decimal Sum(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public decimal Sub(decimal a, decimal b)
+        {
+            return a - b;
+        }
+        public decimal Mult(decimal a, decimal b)
+        {
+            return a * b;
+        }
+        public decimal Div(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+    }
+}
--- ConsoleApplicationProject/Program.cs
+++ ConsoleApplicationProject/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApplicationProject;
+
+
+var usrInput = Console.ReadLine();
+
+decimal output = 0.00000000001M;
+
+while (decimal.TryParse(usrInput, out output) == false || output == 0.00000000001M)
+{
+    Console.WriteLine("Wrong Input");
+    usrInput = Console.ReadLine();
+};
+
+
+
